c53fa38d3c48b0c818f84c7c29715f5c669889a5
Ergun Polat <epolat@rocketlawyer.co.uk>
2016-11-29 15:08:09 +0000

    added mongoose orm

---
+++ server/server.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+//model
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc)=>{
+//   console.log('Saved todo', doc);
+// },(e) => {
+//   console.log('unable to save todo');
+// });
+
+var otherTodo = new Todo({
+  text: 'Something todo',
+  completed: true,
+  completedAt: 123
+});
+
+otherTodo.save().then((doc)=>{
+  console.log(JSON.stringify(doc,undefined,2));
+},(e) => {
+  console.log('unable to save todo');
+});
